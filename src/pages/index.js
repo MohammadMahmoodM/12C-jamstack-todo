9d6d3e47c186e99de51e332b851d502183b5f115
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-08-27 03:04:30 +0500

    some bug cleared

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Todolist from './components/Todolist';
+import Todolist from '../components/Todolist';
 
 export default function Home(){
     return <Todolist/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@
 module.exports = {
   mode: "jit",
   content: ["./*.html"],
-  darkMode: false, // or 'media' or 'class'
+  //darkMode: media, // or 'media' or 'class'
   theme: {
     extend: {},
   },
