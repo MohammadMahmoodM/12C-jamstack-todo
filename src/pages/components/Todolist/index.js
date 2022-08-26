82b32b083e197d58e2750752133d762993791bff
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-08-26 23:20:23 +0500

    added tailwindcss created todolist component

---
+++ src/pages/components/Todolist/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//  Todo List
+
+const Todolist = () => {
+    return (
+        <div>
+            <h1>Todo List</h1>
+        </div>
+    )
+}
+
+export default Todolist;
