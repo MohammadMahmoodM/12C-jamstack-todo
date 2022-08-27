59fdf39367396fc1198dcbdc7e48aa4af298dcb3
mahmoodlegend <mohammadmahmood441@gmail.com>
2022-08-27 12:57:04 +0500

    imported links.gql to faunadb, added first todo to faunadb using ui

--- src/components/Todolist/TodoInput/index.js
+++ src/components/Todolist/TodoInput/index.js
@@ -8,26 +8,41 @@ import { Formik } from "formik"
 const TodoInput = () => {
   return (
     <Formik
-       initialValues={{task: ''}}
-       validate={values => {
+      initialValues={{ task: '' }}
+      validate={values => {
         const errors = {}
         if (!values.task) {
           errors.message = "Required"
         }
         return errors
-       }}
-       onSubmit={ (values, {setSubmitting} ) => {
-         // same shape as initial values
-         console.log("values", values)
-         setSubmitting(false);
-       }}
-     >
-       {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-         <form 
-         onSubmit = {handleSubmit}
-           className = "bg-white flex justify-between items-center shadow-md rounded px-8 pt-6 mt-6 pb-8 mb-4"
-         >
-          <input 
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        // same shape as initial values
+        console.log("values", values)
+        //setSubmitting(false);  isko true kar ky dakhon ga
+
+        // Post Data
+        // fetch(`/.netlify/functions/todo-list`, {
+        //   method: "POST",
+        //   body: JSON.stringify(values),
+        // })
+        //   .then(res => res.json())
+        //   .then(result => {
+        //     console.log("success", result)
+        //     setSubmitting(false) // isko false kay ky dakhon ga
+        //   })
+        //   .catch(err => {
+        //     console.log(err)
+        //   })
+
+      }}
+    >
+      {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white flex justify-between items-center shadow-md rounded px-8 pt-6 mt-6 pb-8 mb-4"
+        >
+          <input
             className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="task"
             type="text"
@@ -36,17 +51,17 @@ const TodoInput = () => {
             onBlur={handleBlur}
             placeholder="Task"
           />
-           {errors.message && touched.message && errors.message}
-           <button
+          {errors.message && touched.message && errors.message}
+          <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
             disabled={isSubmitting}
           >
             {isSubmitting ? "Adding.." : "Add"}
           </button>
-         </form>
-       )}
-     </Formik>
+        </form>
+      )}
+    </Formik>
     // <form
     //   onSubmit={() => alert("Submitted")}
     //   className="bg-white flex justify-between items-center shadow-md rounded px-8 pt-6 mt-6 pb-8 mb-4"
